7e12a402bbfa93fcf7b209c4e980fb7fdcfccd9e
DevEsteban <chetesnico@gmail.com>
2022-10-06 21:13:06 -0500

    se hizo la estructura basica de la cards

--- src/Components/Game/index.jsx
+++ src/Components/Game/index.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import style from './styles.module.css';
 
 const Game = () => {
-    return ( 
-        <h1>GAME</h1>
-     );
-}
- 
-export {Game};
+	const card = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+
+	return (
+		<div className={style.contain}>
+			<div className={style.cards}>
+				{card.map((item, i) => {
+					return (
+						<div key={i} className={style.card}>
+							{item}
+						</div>
+					);
+				})}
+			</div>
+            <div className={style.infogame}>
+
+
+            </div>
+		</div>
+	);
+};
+
+export { Game };
