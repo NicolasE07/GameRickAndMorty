2e80587ac4724aef29269f14cfe7a75b9606513e
laptopMaye <chetesnico@gmail.com>
2022-10-09 13:47:07 -0500

    se agrega peticion get a la api de rick and morty falta mejorar logica para obtener las cards aleatorias

---
+++ src/Components/Card/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Card = (url) => {
+    return ( 
+        <img src={url} alt={name} />
+     );
+}
+ 
+export {Card};
--- src/Components/Home/index.jsx
+++ src/Components/Home/index.jsx
@@ -1,8 +1,34 @@
 import React from 'react';
 import style from './styles.module.css'
 import logo from '../../assets/img/logo.png'
+import axios, { Axios } from 'axios';
 import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 const Home = () => {
+
+	const [characters, setCharacters]=useState(826);
+    const [num, setNums] = useState([]);
+    const consultar = async()=>{
+        const {data} = await axios.get('https://rickandmortyapi.com/api/character')
+        setCharacters(data.info.count)
+        console.log(characters)
+        for(let i =  0; i < 20; i++){
+            const result = charactersRandom(characters)
+            setNums((preState)=> [...preState, result]);
+        }
+
+    }
+    const charactersRandom=(maxCha)=>{
+      const min = Math.ceil(1);
+      const max = Math.floor(maxCha);
+     const result = Math.floor(Math.random() * (max - min) + min)
+     return result; // The maximum is exclusive and the minimum is inclusive
+    }
+    
+    useEffect(()=>{
+        consultar()
+    },[])
+
     return ( 
 
         <div className={style.container}>
